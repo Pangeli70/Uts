babf900511953a439c5e3b939b1625c7ac0153e3
PAngeli70 <pangeli70@gmail.com>
2024-07-14 00:22:12 +0200

    Update std version

--- lib/imports/deno.land.std.ts
+++ lib/imports/deno.land.std.ts
@@ -2,10 +2,11 @@
  * @module [Brd/Uts]
  * @author [APG] Angeli Paolo Giusto
  * @version 0.1 APG 20230418
+ * @version 0.2 APG 20230418 @0.224.0
  * ----------------------------------------------------------------------------
  */
 
 // Deno std
-export * as Fs from "https://deno.land/std@0.182.0/fs/mod.ts";
-export * as Path from "https://deno.land/std@0.182.0/path/mod.ts";
-export type { Cookie } from "https://deno.land/std@0.182.0/http/cookie.ts";
+export * as Fs from "https://deno.land/std@0.224.0/fs/mod.ts";
+export * as Path from "https://deno.land/std@0.224.0/path/mod.ts";
+export type { Cookie } from "https://deno.land/std@0.224.0/http/cookie.ts";
